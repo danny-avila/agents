30acc956ac1b1af091b161075da90d5d2e03853e
Alde Rojas <hello@alde.dev>
2025-09-28 01:04:16 -0500

    fix: use the last stepId for a given stepKey

--- src/tools/handlers.ts
+++ src/tools/handlers.ts
@@ -34,7 +34,7 @@ export async function handleToolCallChunks({
   let prevStepId: string;
   let prevRunStep: t.RunStep | undefined;
   try {
-    prevStepId = graph.getStepIdByKey(stepKey, graph.contentData.length - 1);
+    prevStepId = graph.getStepIdByKey(stepKey);
     prevRunStep = graph.getRunStep(prevStepId);
   } catch {
     /** Edge Case: If no previous step exists, create a new message creation step */
@@ -48,7 +48,7 @@ export async function handleToolCallChunks({
     prevRunStep = graph.getRunStep(prevStepId);
   }
 
-  const _stepId = graph.getStepIdByKey(stepKey, prevRunStep?.index);
+  const _stepId = graph.getStepIdByKey(stepKey);
 
   /** Edge Case: Tool Call Run Step or `tool_call_ids` never dispatched */
   const tool_calls: ToolCall[] | undefined =
